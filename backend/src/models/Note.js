3bcc5a3161e7d646f5fc5eb1f586f53d3af45dc1
palmero-jerome07 <jeromepalmero7@gmail.com>
2026-03-19 19:51:07 +0800

    feat: implement full note management with CRUD operations and reply functionality.

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -3,19 +3,31 @@ import mongoose from "mongoose";
 // 1- create a schema
 // 2- create a model based on the schema
 
-const notesSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        }
-    }, {timestamps: true} // createdAt and updatedAt fields
+const replySchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
 );
 
-const Note = mongoose.model("Note", notesSchema)
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    replies: [replySchema],
+  },
+  { timestamps: true }, // createdAt and updatedAt fields
+);
 
-export default Note
+const Note = mongoose.model("Note", notesSchema);
+
+export default Note;
